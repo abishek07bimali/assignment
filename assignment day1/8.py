024bcbbf74e5d8d30143b0973d220a1716ab8985
abishek07bimali <abi0shekbimali@gmail.com>
2021-12-25 18:50:25 +0545

    update

--- assignment day1/8.py
+++ assignment day1/8.py
@@ -1,9 +1,4 @@
 #8. Write a Python program to convert a tuple to a string. 
- 
-def convert(tup):
-    str = ''.join(tup)
-    return str
-
-
-A=(1,2,3,4,5,)
-print(convert(A))
+A=("A","B","I")
+st = ''.join(A)
+print(st)
