a63aee13468ce3ab2937ab7b56aa7f226aa6c9cd
abishek07bimali <abi0shekbimali@gmail.com>
2021-12-27 22:10:43 +0545

    update

--- assignment day1/3.py
+++ assignment day1/3.py
@@ -1,9 +1,9 @@
 # Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
 
-A=["Abas","Abd","Aba","Asa"]
-B=0
+A=["Abas","Abd","AbA","AsA"]
 
+B=0
 for word in A:
-    if len(word)>1 (word[0]==word[-1]):
+    if len(word)>1 and word[0]==word[-1]:
         B=B+1
-print(B)
+        print(B)
